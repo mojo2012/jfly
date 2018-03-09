211f91e38888c1bcc8d7e3e80d4a996d2ede6c33
Matthias Fuchs <meister.fuchs@gmail.com>
2018-03-09 16:05:00 +0100

    added listview prototype

---
+++ src/main/java/at/spot/jfly/ui/navigation/ListGroupItem.java
@@ -0,0 +1,11 @@
+package at.spot.jfly.ui.navigation;
+
+import at.spot.jfly.ComponentHandler;
+
+public class ListGroupItem extends ListValueItem {
+
+	public ListGroupItem(ComponentHandler handler, String text) {
+		super(handler, text);
+	}
+
+}
---
+++ src/main/java/at/spot/jfly/ui/navigation/ListView.java
@@ -0,0 +1,11 @@
+package at.spot.jfly.ui.navigation;
+
+import at.spot.jfly.ComponentHandler;
+import at.spot.jfly.ui.base.AbstractContainerComponent;
+
+public class ListView extends AbstractContainerComponent<ListValueItem> {
+
+	public ListView(ComponentHandler handler) {
+		super(handler);
+	}
+}
