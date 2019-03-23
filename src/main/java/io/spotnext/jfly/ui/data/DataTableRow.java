738c53977679909e4a41873874a62ef9fef631d5
Matthias Fuchs <meister.fuchs@gmail.com>
2019-03-23 21:13:35 +0100

    datarow implements comparable

--- src/main/java/io/spotnext/jfly/ui/data/DataTableRow.java
+++ src/main/java/io/spotnext/jfly/ui/data/DataTableRow.java
@@ -1,10 +1,19 @@
 package io.spotnext.jfly.ui.data;
 
-public interface DataTableRow {
+public interface DataTableRow extends Comparable<DataTableRow> {
 
 	String getId();
 
 	boolean isSelected();
 
 	void setSelected(boolean selected);
+
+	@Override
+	default int compareTo(DataTableRow o) {
+		if (o == null) {
+			return -1;
+		}
+
+		return getId().compareTo(o.getId());
+	}
 }
