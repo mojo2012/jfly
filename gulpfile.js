0ea65b14abb91e2857332b771b3b491bef619953
Matthias Fuchs <meister.fuchs@gmail.com>
2019-03-31 21:29:48 +0200

    fixed eclipse npm build

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+const fs   = require('fs');
+var concat = require('gulp-concat');
+
+gulp.task('default', (done) => {
+
+    const folders = [
+    	'./target/',
+    	'./target/classes/',
+    	'./target/classes/public/',
+        './target/classes/public/script',
+        './target/classes/public/style'
+    ];
+
+    folders.forEach(dir => {
+        if(!fs.existsSync(dir)) {
+            fs.mkdirSync(dir);
+        }   
+    });
+
+    gulp.src([
+    		'node_modules/jquery/dist/jquery.min.js',
+    		'node_modules/vue/dist/vue.min.js',
+    		'node_modules/vuetify/dist/vuetify.min.js'
+		])
+	    .pipe(concat('deps.min.js'))
+	    .pipe(gulp.dest('./target/classes/public/script/'));
+
+	gulp.src([
+    		'node_modules/vuetify/dist/vuetify.min.css',
+    		'node_modules/material-design-icons-iconfont/dist/material-design-icons.css'
+		])
+	    .pipe(concat('deps.css'))
+	    .pipe(gulp.dest('./target/classes/public/style/'));
+
+    done();
+});
