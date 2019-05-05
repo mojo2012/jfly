a2c10677526414ec2f623862aec45ad686a2a039
Matthias Fuchs <meister.fuchs@gmail.com>
2019-05-05 14:07:56 +0200

    removed vue-material dependency

--- gulpfile.js
+++ gulpfile.js
@@ -18,25 +18,28 @@ gulp.task('default', (done) => {
         }   
     });
 
+    // regular JS
     gulp.src([
 		'node_modules/jquery/dist/jquery.min.js',
 		'node_modules/vue/dist/vue.js',
-		'node_modules/vuetify/dist/vuetify.js'
+		'node_modules/vuetify/dist/vuetify.js',
 	])
     .pipe(concat('deps.js'))
     .pipe(gulp.dest('./target/classes/public/script/'));
     
+    // minified JS
     gulp.src([
     		'node_modules/jquery/dist/jquery.min.js',
     		'node_modules/vue/dist/vue.min.js',
-    		'node_modules/vuetify/dist/vuetify.min.js'
+    		'node_modules/vuetify/dist/vuetify.min.js',
 		])
 	    .pipe(concat('deps.min.js'))
 	    .pipe(gulp.dest('./target/classes/public/script/'));
 
+    // minified CSS
 	gulp.src([
     		'node_modules/vuetify/dist/vuetify.min.css',
-    		'node_modules/material-design-icons-iconfont/dist/material-design-icons.css'
+    		'node_modules/material-design-icons-iconfont/dist/material-design-icons.css',
 		])
 	    .pipe(concat('deps.css'))
 	    .pipe(gulp.dest('./target/classes/public/style/'));
